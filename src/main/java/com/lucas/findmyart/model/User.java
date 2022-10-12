f8651b7fdd45a4958c62ae3791d80cf96caf2c1d
hgrafa <hugorafell@gmail.com>
2022-10-12 19:32:53 -0300

    Creating User model

---
+++ src/main/java/com/lucas/findmyart/model/User.java
@@ -0,0 +1,29 @@
+package com.lucas.findmyart.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class User {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String username;
+  private String email;
+  private String password;
+  private Boolean enabled;
+
+}
