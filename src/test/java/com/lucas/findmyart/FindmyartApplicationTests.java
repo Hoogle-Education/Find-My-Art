e11161c8915039a8b8a14cf868e12ecfa8442663
hgrafa <hugorafell@gmail.com>
2022-10-12 19:33:42 -0300

    Preparing Unitary Tests File

---
+++ src/test/java/com/lucas/findmyart/FindmyartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.findmyart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmyartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
