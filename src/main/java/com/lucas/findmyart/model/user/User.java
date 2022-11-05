8446b43ecb5b297932315defb0dc7d34f88a82a1
hgrafa <hugorafell@gmail.com>
2022-11-05 05:14:32 -0300

    Updating application properties

--- src/main/java/com/lucas/findmyart/model/user/User.java
+++ src/main/java/com/lucas/findmyart/model/user/User.java
@@ -1,10 +1,6 @@
 package com.lucas.findmyart.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,17 +9,22 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "users")
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter @NoArgsConstructor
 public class User {
   
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  private String profilePhoto;
+
   private String username;
   private String email;
   private String password;
   private Boolean enabled;
+
+  @OneToOne
+  @JoinColumn(name = "authority_id")
+  private Authority authority;
 
 }
