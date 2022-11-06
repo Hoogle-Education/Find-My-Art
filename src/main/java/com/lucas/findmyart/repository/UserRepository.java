9b2502121df218b2e5683f9379932ab31b82dcdd
hgrafa <hugorafell@gmail.com>
2022-11-06 17:15:50 -0300

    Updating User Repository

--- src/main/java/com/lucas/findmyart/repository/UserRepository.java
+++ src/main/java/com/lucas/findmyart/repository/UserRepository.java
@@ -3,9 +3,12 @@ package com.lucas.findmyart.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.lucas.findmyart.model.User;
+import com.lucas.findmyart.model.user.User;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-  
+    Optional<User> findByUsername(String username);
+
 }
