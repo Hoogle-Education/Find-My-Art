73b570119ef999d90fc96106f5477944f839a359
hgrafa <hugorafell@gmail.com>
2022-11-06 17:29:02 -0300

    Creating Payment initial model

---
+++ src/main/java/com/lucas/findmyart/model/order/Payment.java
@@ -0,0 +1,29 @@
+package com.lucas.findmyart.model.order;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payments")
+@Getter @Setter @NoArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime moment;
+
+    @JsonIgnore
+    @OneToOne
+    @MapsId
+    private Order order;
+
+}
