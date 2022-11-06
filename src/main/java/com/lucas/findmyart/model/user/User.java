1d5ce9148d8e9d122a0b6724b9bb6e015930af75
hgrafa <hugorafell@gmail.com>
2022-11-06 17:17:32 -0300

    Updating User entity attributues

--- src/main/java/com/lucas/findmyart/model/user/User.java
+++ src/main/java/com/lucas/findmyart/model/user/User.java
@@ -1,11 +1,16 @@
-package com.lucas.findmyart.model;
+package com.lucas.findmyart.model.user;
 
 import javax.persistence.*;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.lucas.findmyart.model.order.Order;
+import com.lucas.findmyart.model.order.Product;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -16,7 +21,7 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private String profilePhoto;
+  private String profilePhotoToken;
 
   private String username;
   private String email;
@@ -26,5 +31,9 @@ public class User {
   @OneToOne
   @JoinColumn(name = "authority_id")
   private Authority authority;
+
+  @JsonIgnore
+  @OneToMany(mappedBy = "client")
+  private List<Order> orders = new ArrayList<>();
 
 }
