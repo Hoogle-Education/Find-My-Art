a54610198a101d96b849bdc4c68c9747dbf66b2b
hgrafa <hugorafell@gmail.com>
2022-11-07 23:55:42 -0300

    Adding dto resource models

---
+++ src/main/java/com/lucas/findmyart/api/dto/UserDto.java
@@ -0,0 +1,22 @@
+package com.lucas.findmyart.api.dto;
+
+import com.lucas.findmyart.model.user.User;
+import com.lucas.findmyart.util.GravatarTokenFactory;
+import lombok.Getter;
+import lombok.Setter;
+@Getter @Setter
+public class UserDto {
+
+    private String profileImageToken;
+    private String username;
+    private String email;
+    private Boolean enabled;
+
+    public UserDto(User user) {
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.enabled = user.getEnabled();
+        this.profileImageToken = GravatarTokenFactory.gravatarHex(email);
+    }
+
+}
