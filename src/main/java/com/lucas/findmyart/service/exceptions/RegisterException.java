8a50dccc56fe6b21d550fe47da3266cac4b9756d
hgrafa <hugorafell@gmail.com>
2022-11-08 21:51:30 -0300

    Adding Register Exception

---
+++ src/main/java/com/lucas/findmyart/service/exceptions/RegisterException.java
@@ -0,0 +1,8 @@
+package com.lucas.findmyart.service.exceptions;
+
+public class RegisterException extends RuntimeException {
+
+    public RegisterException(String message) {
+        super(message);
+    }
+}
