51474e5f7e646432a4ee5cda2b23dc7b40ff5868
hgrafa <hugorafell@gmail.com>
2022-11-10 18:37:05 -0300

    Creating Band Service

---
+++ src/main/java/com/lucas/findmyart/service/BandService.java
@@ -0,0 +1,15 @@
+package com.lucas.findmyart.service;
+
+import com.lucas.findmyart.repository.BandRepository;
+import com.lucas.findmyart.repository.PubRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BandService {
+
+    private BandRepository bandRepository;
+    private PubRepository pubRepository;
+
+
+
+}
