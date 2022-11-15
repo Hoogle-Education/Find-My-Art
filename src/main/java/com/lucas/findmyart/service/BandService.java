60dc560a979c4b4e8dfc4805dac971e6460169c4
hgrafa <hugorafell@gmail.com>
2022-11-15 17:25:43 -0300

    Updating Band Service

--- src/main/java/com/lucas/findmyart/service/BandService.java
+++ src/main/java/com/lucas/findmyart/service/BandService.java
@@ -2,9 +2,11 @@ package com.lucas.findmyart.service;
 
 import com.lucas.findmyart.repository.BandRepository;
 import com.lucas.findmyart.repository.PubRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class BandService {
 
     private BandRepository bandRepository;
